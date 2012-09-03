3f6eeb0eafe292e7ff3c498f8183f9ec740c984e
xenon <297773034@qq.com>
2012-09-03 20:24:53 +0800

    Signed-off-by: xenon <297773034@qq.com>

--- weibo/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=UTF-8');
-
-define( "WB_AKEY" , '1973619647' );
-define( "WB_SKEY" , '376b127c226e214cd49c595d61682870' );
-define( "WB_CALLBACK_URL" , 'http://localhost/weibo/callback.php' );
