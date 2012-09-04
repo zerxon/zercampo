311d4ae451d4a9c536b4df642d0522d68256cdae
xenon <297773034@qq.com>
2012-09-04 09:17:04 +0800

    update

--- backstage/templates_c/%%5D^5DA^5DAB0DC3%%footer.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2012-08-05 04:23:41
-         compiled from ../../templates/footer.tpl */ ?>
-  <!-- Placed at the end of the document so the pages load faster -->
-  <script src="/js/jquery-1.7.2.min.js"></script>
-  <script src="/js/bootstrap.min.js"></script>
-</body>
